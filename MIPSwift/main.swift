fdd2745bba7448e61d8a3875f551021170721146
Hunter Knepshield <theragingfatcat@gmail.com>
2015-12-10 01:25:09 -0500

    Initial Commit

---
+++ MIPSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MIPSwift
+//
+//  Created by Hunter Knepshield on 12/10/15.
+//  Copyright © 2015 Hunter Knepshield. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
